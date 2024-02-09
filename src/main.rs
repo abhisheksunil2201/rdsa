3ddea78163987ec0fa064bc04ec0c6fa26337cc0
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-02-09 10:44:44 +0530

    add queue

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 extern crate lazy_static;
+pub mod data_structures;
 pub mod searching;
 
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::searching;
-    #[test]
-    fn binary_search() {
-        let arr = vec!["zoo", "google", "d", "c", "b", "a"];
-        let index = searching::binary_search(&"a", &arr);
-        assert_eq!(index, Some(5));
-    }
 }
